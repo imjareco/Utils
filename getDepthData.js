d319bef0d72ecba1dee8298b954721ebabda4c51
Jhony Dev <24277595+imjareco@users.noreply.github.com>
2021-02-20 01:45:28 +0100

    Create getDepthData.js
    Function to get child property by an object, following a path.

---
+++ getDepthData.js
@@ -0,0 +1,26 @@
+/* 
+  Function to get child property by an object, following a path.
+  @params path data.user.profile.name with this path we get "Jhony" the name value.
+  @params obj:
+  data: {
+    messages: ["string"],
+    user: {
+      profile: {
+        _id: "string",
+        name: "Jhony",
+        surname: "string
+      }
+    }
+  }
+  @return The value of the last property specified in the path. In this case "name"
+*/
+const getDepthData = (path, obj) => {
+    let { Data: objData } = obj
+    const [, ...keyList] = path.split('.')
+    for (let key of keyList) {
+        if (objData[key] !== undefined) {
+            objData = objData[key]
+        }
+    }
+    return objData
+}
